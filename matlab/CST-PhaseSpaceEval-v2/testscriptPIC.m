0fadab402ddbca7b6c07a33764a51b1455cb8237
HPLegion <32222819+HPLegion@users.noreply.github.com>
2017-09-27 11:39:06 +0200

    adjust script for current tests

--- matlab/CST-PhaseSpaceEval-v2/testscriptPIC.m
+++ matlab/CST-PhaseSpaceEval-v2/testscriptPIC.m
@@ -1,6 +1,6 @@
 %% Set Up
-filepath = './';
-filename = 'tst2.txt';
+filepath = 'M:\evaltest\split\';
+filename = '12C_gauss_rx_3_ry_3_x_0_y_0.txt';
 des_filename = 'centraltrajectory_intf.txt';
 dtr_id = 0; %id of particle following design trajectory (dtr)
 SID =0;
@@ -9,22 +9,22 @@ SID =0;
 [p_id, p_sid, p_time, ps_data] =...
     importCstTrajectoriesPIC(filename,filepath);
 
-[fp_id, fp_sid, fp_time, fps_data, ffirst_index]...
-    = filterPIC(SID, p_id, p_sid, p_time, ps_data);
+% [fp_id, fp_sid, fp_time, fps_data, ffirst_index]...
+%     = filterPIC(SID, p_id, p_sid, p_time, ps_data);
 
 % [p_id, p_time, ps_data] =...
 %     insertDesignTrajectory(p_id, p_time, ps_data, des_filename, filepath);
 
-dtr_ind = find([fp_id{:}] == ffirst_index); %find dtr Matlab index
+dtr_ind = find([p_id{:}] == dtr_id); %find dtr Matlab index
 clear p_id dtr_id
 
 %% Get Splines for x y z npx npy npz
-fps_splines = fitSplines(fp_time, fps_data);
+ps_splines = fitSplines(p_time, ps_data);
 
 clear ps_data %Only work using splines from here
 
 %% Obtain time limits for each particle and design tracejtory (dtr)
-time_lim = findTimeRanges(fp_time, dtr_ind);
+time_lim = findTimeRanges(p_time, dtr_ind);
 
-clear p_time
-phaseSpacePlotter(time_lim, fps_splines, dtr_ind,filename)
+% clear p_time
+phaseSpacePlotter(time_lim, ps_splines, dtr_ind,filename)
