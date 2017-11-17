86b6e13029fdc53cc41e52f489c3c32df1070d2e
HPLegion <32222819+HPLegion@users.noreply.github.com>
2017-11-17 14:09:50 +0100

    Implement Event Record and clean up code

--- python/PhaseSpaceEval/particlemonitor.py
+++ python/PhaseSpaceEval/particlemonitor.py
@@ -2,8 +2,8 @@
 Contains a class that represents a particle monitor
 """
 import numpy as np
-from scipy import interpolate
 from scipy import optimize
+from pandas import DataFrame
 
 class ParticleMonitor:
     """
@@ -16,7 +16,8 @@ class ParticleMonitor:
                  abs_vel=0, abs_mom=0):
         """
         Initialiser for the ParticleMonitor Object
-        can either manually define the required parameters or hand over a trajectory from which they will be computed
+        can either manually define the required parameters or
+        hand over a trajectory from which they will be computed
         A time is always required!
 
         Keyword Args:
@@ -28,7 +29,8 @@ class ParticleMonitor:
         """
 
         self.time0 = time0
-
+        #Create empty list for events to be saved in
+        self.__events = list()
         # If no trajectory is given use defaults or manual user input
         if trajectory == None:
             self.r0 = r0.copy()
@@ -75,50 +77,58 @@ class ParticleMonitor:
         if lb == None:
             lb = trajectory.tmin
         else:
-            lb = np.maximum(trajectory.tmin,lb)
+            lb = np.maximum(trajectory.tmin, lb)
         if ub == None:
             ub = trajectory.tmax
         else:
-            ub = np.minimum(trajectory.tmin,lb)
-        
+            ub = np.minimum(trajectory.tmin, lb)
+
         # dot product of vector difference (traj_pos - monitor_pos) with w
         def f(t):
             return (trajectory.interp_pos(t)-self.r0).dot(self.w)
         # find root of f to find intersection
-        (t_c, s_info) = optimize.brenth(f,lb,ub,full_output = True)
-        
+        (t_event, s_info) = optimize.brenth(f, lb, ub, full_output=True)
+
         # if no solution was found,i.e. the particle does not reach the monitor
         if not s_info.converged:
             return -1
-        
+
         # Calculate u(i.e. x) and v(i.e. y) component of intersection in the monitor coordinates
-        ps_u = (trajectory.interp_pos(t_c)-self.r0).dot(self.u)
-        ps_v = (trajectory.interp_pos(t_c)-self.r0).dot(self.v)
+        ps_u = (trajectory.interp_pos(t_event)-self.r0).dot(self.u)
+        ps_v = (trajectory.interp_pos(t_event)-self.r0).dot(self.v)
 
         # Calculate longitudinal displacement
-        ps_l = -self.abs_vel*(t_c-self.time0)
+        ps_l = -self.abs_vel*(t_event-self.time0)
 
         # Calculate up(i.e. x') and vp(i.e. y')
-        uvw_mom = trajectory.interp_mom(t_c).dot(self.rotmat)
-        ps_up = 1000*np.arctan(uvw_mom[0]/uvw_mom[2])
-        ps_vp = 1000*np.arctan(uvw_mom[1]/uvw_mom[2])
+        uvw_mom = trajectory.interp_mom(t_event).dot(self.rotmat)
+        ps_up = 1000*np.arctan2(uvw_mom[0], uvw_mom[2])
+        ps_vp = 1000*np.arctan2(uvw_mom[1], uvw_mom[2])
 
         # momentum spread
         ps_delta = (np.linalg.norm(uvw_mom)-self.abs_mom)/self.abs_mom
 
-        inters_data = [trajectory.particleID, trajectory.sourceID, t_c,
+        inters_data = [trajectory.particleID, trajectory.sourceID, t_event,
                        ps_u, ps_up, ps_v, ps_vp, ps_l, ps_delta]
         return inters_data
 
-    
-    def store_intersect(self, trajectory, lb=None, ub=None):
+
+    def record_intersect(self, trajectory, lb=None, ub=None):
         """
-        finds the intersection of trajectory with this monitor and stores the event in a dataframe that can be accessed with
-        get_events, the return value of find_intersect is returned as well
+        finds the intersection of trajectory with this monitor and stores the event internally
+        A dataframe of all recorded events can be accessed with
+        get_events(), the return value of find_intersect is returned as well
         """
 
-        result = self.find_intersect(trajectory, lb=None, ub=None)
-
-        #TOBEIMPLEMENTED
-
+        result = self.find_intersect(trajectory, lb, ub)
+        # Appends the list of results to the event list containg all result lists
+        self.__events.append(result.copy())
         return result
+
+    def get_events(self):
+        """
+        Returns a dataframe with all events that have been recorded by this monitor
+        """
+        colnames = ["particleID", "sourceID", "t_event",
+                    "ps_u", "ps_up", "ps_v", "ps_vp", "ps_l", "ps_delta"]
+        return DataFrame(self.__events, columns=colnames)
